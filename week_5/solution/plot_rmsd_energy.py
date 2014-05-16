e6bcad3bc52ee17fdbeeed03807c62e5c76b54b2
Lars Andersen Bratholm <lab@sunray.theory.ki.ku.dk>
2014-05-16 16:41:52 +0200

    made script to plot rmsd/energy data

---
+++ week_5/solution/plot_rmsd_energy.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import sys
+
+native_filename = sys.argv[1]
+non_native_filename = sys.argv[2]
+
+native_lines = open(native_filename,"r").readlines()
+non_native_lines = open(non_native_filename,"r").readlines()
+
+native_rmsd = [line.split()[0] for line in native_lines]
+native_data = [line.split()[1] for line in native_lines]
+
+non_native_rmsd = [line.split()[0] for line in non_native_lines]
+non_native_data = [line.split()[1] for line in non_native_lines]
+
+
+plt.plot(non_native_rmsd,non_native_data,".k",markeredgecolor='k')
+plt.plot(native_rmsd,native_data,".r",markeredgecolor='r')
+plt.savefig("test.png")
