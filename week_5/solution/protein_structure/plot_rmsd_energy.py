4d7ce4fb86b407ac0abc71c4d837beb2f9fa4578
Lars Andersen Bratholm <larsbratholm@gmail.com>
2014-05-22 11:16:08 +0200

    changes to chemical shift solution

--- week_5/solution/protein_structure/plot_rmsd_energy.py
+++ week_5/solution/protein_structure/plot_rmsd_energy.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import sys
 
+# See chemical shift exercise for explanation of sys.argv
 native_filename = sys.argv[1]
 non_native_filename = sys.argv[2]
 
@@ -13,7 +14,6 @@ native_data = [line.split()[1] for line in native_lines]
 non_native_rmsd = [line.split()[0] for line in non_native_lines]
 non_native_data = [line.split()[1] for line in non_native_lines]
 
-
 plt.plot(non_native_rmsd,non_native_data,".k",markeredgecolor='k')
 plt.plot(native_rmsd,native_data,".r",markeredgecolor='r')
-plt.savefig("test.png")
+plt.savefig("rmsd_E.png")
