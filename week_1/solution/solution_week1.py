6a57d79f1787c9d2dd60b1e256396e307e8a0e08
Jimmy Charnley Kromann <jimmy@charnley.dk>
2015-01-28 14:31:29 +0100

    longer variable names

--- week_1/solution/solution_week1.py
+++ week_1/solution/solution_week1.py
@@ -9,15 +9,15 @@ n_steps = 1000
 dt = 0.01
 
 # create the x- and y-coordinates
-pos_x = [random.random() for i in range(n_particles)]
-pos_y = [(random.random()-0.5)*2 for i in range(n_particles)]
+positions_x = [random.random() for i in range(n_particles)]
+positions_y = [(random.random()-0.5)*2 for i in range(n_particles)]
 
 # create the velocities
-vel_x = [(random.random()-0.5)*2 for i in range(n_particles)]
-vel_y = [(random.random()-0.5)*2 for i in range(n_particles)]
+velocities_x = [(random.random()-0.5)*2 for i in range(n_particles)]
+velocities_y = [(random.random()-0.5)*2 for i in range(n_particles)]
 
 # Plot the x- and y- coordinates
-plt.plot(pos_x, pos_y, 'ro')
+plt.plot(positions_x, positions_y, 'ro')
 plt.axis((-1, 1, -1, 1))
 plt.savefig("coordinates_start.png")
 
@@ -29,28 +29,29 @@ for n in range(n_steps):
     for i in range(n_particles):
 
         # make a step in time dt
-        pos_x[i] = pos_x[i] + vel_x[i]*dt
-        pos_y[i] = pos_y[i] + vel_y[i]*dt
+        positions_x[i] = positions_x[i] + velocities_x[i]*dt
+        positions_y[i] = positions_y[i] + velocities_y[i]*dt
 
         # if the particle is exiting the box in the x-direction, change the
         # velocity
-        if abs(pos_x[i] + vel_x[i]*dt) > 1.0:
-            vel_x[i] = -vel_x[i]
-            pos_x[i] = pos_x[i] + vel_x[i]*dt
+        if abs(positions_x[i] + velocities_x[i]*dt) > 1.0:
+            velocities_x[i] = -velocities_x[i]
+            positions_x[i] = positions_x[i] + velocities_x[i]*dt
 
-        if abs(pos_y[i] + vel_y[i]*dt) > 1.0:
-            vel_y[i] = -vel_y[i]
-            pos_y[i] = pos_y[i] + vel_y[i]*dt
+
+        if abs(positions_y[i] + velocities_y[i]*dt) > 1.0:
+            velocities_y[i] = -velocities_y[i]
+            positions_y[i] = positions_y[i] + velocities_y[i]*dt
 
 
     # save a 'frame' for the video every 10 step
     if n % 10 == 0:
-        video.add_frame(pos_x, pos_y)
+        video.add_frame(positions_x, positions_y)
 
 
 # Plot the x- and y- coordinates
 plt.clf() # clear figure
-plt.plot(pos_x, pos_y, 'ro')
+plt.plot(positions_x, positions_y, 'ro')
 plt.axis((-1, 1, -1, 1))
 plt.savefig("coordinates_end.png")
 plt.clf()
