514606eb0351c3b430a06a9ea3fbd8e8e6a3296f
Jimmy Charnley Kromann <jimmy@charnley.dk>
2015-04-01 09:36:10 +0200

    solution 1 cleanup

--- solutions/solution_week1.py
+++ solutions/solution_week1.py
@@ -1,24 +1,25 @@
 # Import pyplot and a function to create random numbers
-import random
+import numpy as np
 import matplotlib.pyplot as plt
-import video
+import md_video as video
 
 # initialize some variables
 n_particles = 100
-n_steps = 1000
-dt = 0.01
+box_width = 10.0
+n_steps = 5000
+dt = 0.001
 
 # create the x- and y-coordinates
-positions_x = [random.random() for i in range(n_particles)]
-positions_y = [(random.random()-0.5)*2 for i in range(n_particles)]
+positions_x = [np.random.random()*10 for i in range(n_particles)]
+positions_y = [(np.random.random()-0.5)*2*10 for i in range(n_particles)]
 
 # create the velocities
-velocities_x = [(random.random()-0.5)*2 for i in range(n_particles)]
-velocities_y = [(random.random()-0.5)*2 for i in range(n_particles)]
+velocities_x = [(np.random.random()-0.5)*2*10 for i in range(n_particles)]
+velocities_y = [(np.random.random()-0.5)*2*10 for i in range(n_particles)]
 
 # Plot the x- and y- coordinates
 plt.plot(positions_x, positions_y, 'ro')
-plt.axis((-1, 1, -1, 1))
+plt.axis((-box_width, box_width, -box_width, box_width))
 plt.savefig("coordinates_start.png")
 
 # Make n_steps steps
@@ -34,12 +35,12 @@ for n in range(n_steps):
 
         # if the particle is exiting the box in the x-direction, change the
         # velocity
-        if abs(positions_x[i] + velocities_x[i]*dt) > 1.0:
+        if abs(positions_x[i] + velocities_x[i]*dt) > box_width:
             velocities_x[i] = -velocities_x[i]
             positions_x[i] = positions_x[i] + velocities_x[i]*dt
 
 
-        if abs(positions_y[i] + velocities_y[i]*dt) > 1.0:
+        if abs(positions_y[i] + velocities_y[i]*dt) > box_width:
             velocities_y[i] = -velocities_y[i]
             positions_y[i] = positions_y[i] + velocities_y[i]*dt
 
@@ -47,6 +48,9 @@ for n in range(n_steps):
     # save a 'frame' for the video every 10 step
     if n % 10 == 0:
         video.add_frame(positions_x, positions_y)
+
+    if n % 100 == 0:
+        print 'step', n
 
 
 # Plot the x- and y- coordinates
