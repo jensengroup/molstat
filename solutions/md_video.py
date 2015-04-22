3da58e2f37c9f8aa3450d0fc00e98df35d515540
Jimmy Charnley Kromann <jimmy@charnley.dk>
2015-04-22 10:49:22 +0200

    added boundary conditions

--- solutions/md_video.py
+++ solutions/md_video.py
@@ -2,8 +2,10 @@
 
 import copy
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+
 
 """
 
@@ -21,26 +23,52 @@ c_frames = []
 
 green = "#4daf4a"
 
+mpl.colors.ColorConverter.colors['r'] = (0.8941176470588236, 0.10196078431372549, 0.10980392156862745)
+mpl.colors.ColorConverter.colors['b'] = (0.21568627450980393, 0.49411764705882355, 0.7215686274509804)
+mpl.colors.ColorConverter.colors['g'] = (0.30196078431372547, 0.6862745098039216, 0.2901960784313726)
+mpl.colors.ColorConverter.colors['p'] = (0.596078431372549, 0.3058823529411765, 0.6392156862745098)
+mpl.colors.ColorConverter.colors['y'] = (1.0, 0.4980392156862745, 0.0)
 
-def create_plot(box_width, grid):
+
+def create_plot(box_width, grid, boundary_condition):
     """
     """
 
     fig = plt.figure()
-    ax = fig.add_subplot(111,
-                        aspect='equal',
-                        autoscale_on=False,
-                        xlim=(-(box_width+0.0), (box_width+0.0)),
-                        ylim=(-(box_width+0.0), box_width+0.0))
 
-    # Add an retangle around the box
-    rect = plt.Rectangle((-box_width, -box_width),
-                        2*box_width,
-                        2*box_width,
-                        fill=False,
-                        linewidth=3.0,
-                        edgecolor='k')
-    ax.add_patch(rect)
+    if boundary_condition:
+        ax = fig.add_subplot(111,
+                            aspect='equal',
+                            autoscale_on=False,
+                            xlim=(0, box_width),
+                            ylim=(0, box_width))
+
+        # Add an retangle around the box
+        rect = plt.Rectangle((0, 0),
+                            box_width,
+                            box_width,
+                            fill=False,
+                            linewidth=3.0,
+                            edgecolor='k')
+        ax.add_patch(rect)
+
+    else:
+        ax = fig.add_subplot(111,
+                            aspect='equal',
+                            autoscale_on=False,
+                            xlim=(-(box_width+0.0), (box_width+0.0)),
+                            ylim=(-(box_width+0.0), box_width+0.0))
+
+        # Add an retangle around the box
+        rect = plt.Rectangle((-box_width, -box_width),
+                            2*box_width,
+                            2*box_width,
+                            fill=False,
+                            linewidth=3.0,
+                            edgecolor='k')
+        ax.add_patch(rect)
+
+
 
     if grid:
         ax.grid(True)
@@ -62,28 +90,38 @@ def set_particle_radius():
     """
 
 
-def save(filename, box_width=10.0, fps=25, grid=False, area=300):
+def save(filename, box_width=10.0, fps=25, grid=False, area=300, periodic_boundary=False):
     """
     Takes 2D coordinates x_frames and y_frames, saves it into filename.mp4 with
     len(x_frames) steps.
+
+    # alt_bw alternative box_width (goes from 0..box_width, instead of
+    # -box_width, box_width)
+
     """
 
     global x_frames
     global y_frames
     global c_frames
 
-    color_update = True
-
     x_frames = np.array(x_frames)
     y_frames = np.array(y_frames)
 
+    if periodic_boundary:
+        x_frames = x_frames - box_width * np.floor(x_frames/box_width)
+        y_frames = y_frames - box_width * np.floor(y_frames/box_width)
+
     video_frames = len(x_frames)
 
+    color_update = True
     if len(c_frames) == 0:
         c_frames = [green for n in xrange(video_frames)]
         color_update = False
 
-    fig, ax = create_plot(box_width, grid)
+    if len(c_frames) == 1:
+        color_update = False
+
+    fig, ax = create_plot(box_width, grid, periodic_boundary)
 
     # marker="*"
     # marker='$\heartsuit$'
@@ -135,14 +173,11 @@ def save(filename, box_width=10.0, fps=25, grid=False, area=300):
     # Save as gif
     # ani.save(filename+'.gif', writer='imagemagick')
 
-
     # Clear figure
     plt.clf()
 
 
-
-
-def screenshot(filename, x, y, color=green, box_width=10.0, grid=True, area=300):
+def screenshot(filename, x, y, color=green, box_width=10.0, grid=True, area=300, periodic_boundary=False):
     """
     """
 
@@ -151,7 +186,12 @@ def screenshot(filename, x, y, color=green, box_width=10.0, grid=True, area=300)
     # area = np.pi * radius**2
     # print area
 
-    fig, ax = create_plot(box_width, grid)
+    if periodic_boundary:
+        x = x - box_width * np.floor(x/box_width)
+        y = y - box_width * np.floor(y/box_width)
+
+
+    fig, ax = create_plot(box_width, grid, periodic_boundary)
     ax.scatter(x, y, s=area, c=color, linewidth=0.6)
     ax.plot(x, y, 'ko', ms=6)
 
@@ -176,7 +216,7 @@ def add_frame(x_frame, y_frame):
     y_frames.append(copy.copy(y_frame))
 
 
-def add_color_frame(color):
+def add_color(color):
     """
     Add particle color for a video frame
     """
@@ -201,5 +241,4 @@ if __name__ == "__main__":
                   np.array([2 * (np.random.random() - 0.5) for i in range(n_particles)]) * 10.0)
 
     save('random_particles', 10.0)
-
 
