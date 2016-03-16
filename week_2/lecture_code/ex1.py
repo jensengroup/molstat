b33cd60a2791f3188ff7e6e6509112f4521c266e
Jimmy Charnley Kromann <jimmy@charnley.dk>
2016-03-16 15:46:00 +0100

    clean

--- week_2/lecture_code/ex1.py
+++
@@ -1,9 +0,0 @@
-
-# Example 1
-
-# Week 1 problems
-
-# TODO Find some problems for week 1
-
-
-
--- week_2/lecture_code/ex4.py
+++
@@ -1,9 +0,0 @@
-
-# Example 4, exercise code
-
-# - Fill out functions
-# - Always keep functions in the beginning
-#   and initialize later
-# - don't use constants in the functions there are defined outside
-
-
