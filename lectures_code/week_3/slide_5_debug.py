3bcc68581c8697929f02022ad256bd27989a258e
festorm <freja.storm@chem.ku.dk>
2017-05-17 13:50:31 +0200

    additional notes

--- lectures_code/week_3/slide_5_debug.py
+++ lectures_code/week_3/slide_5_debug.py
@@ -9,8 +9,8 @@ def f(x, y):
     return k
     return l
 
-a, b = f(2.0, 2.0)
-print a, b
+#a, b = f(2.0, 2.0)
+#print a, b
 
 
 # 02
@@ -24,16 +24,19 @@ def random_move(x_pos, y_pos, n_particles):
     for i in range(n_particles):
         x_pos[i] += random.random()
         y_pos[i] += random.random()
-        return y_pos, x_pos
+    return y_pos, x_pos #wrong return
 
 n_particles = 4
 X = [random.random() for x in range(n_particles)]
 Y = [random.random() for y in range(n_particles)]
 
 for n in range(25):
-    x_pos, y_pos = random_move(x_pos, y_pos, n_particles)
+    x_pos, y_pos = random_move(x_pos, y_pos, n_particles) #local variables
+    # x_pos, y_pos = random_move(X, Y, n_particles) #still overwrites X and Y
+    # since these are list and then identical to x_pos and y_pos
 
 
+"""
 # 03
 # - wrong lists (not copy)
 import random
@@ -54,4 +57,4 @@ print pos_x
 pos_x, dx = move(pos_x, 3)
 print pos_x
 print dx
-
+"""
