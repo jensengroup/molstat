c2b83305890c402e25f1c11bd60ccfb51866fb6f
freja <freja.storm@chem.ku.dk>
2017-05-18 09:03:20 +0200

    removed print statement

--- lectures_code/week_4/bubblesort.py
+++ lectures_code/week_4/bubblesort.py
@@ -9,6 +9,7 @@ def bubblesort(X):
     N = len(X)
     for end in range(N, 1, -1):
         for i in range(end - 1):
+            
             cur = X[i]
             if cur > X[i + 1]:
                 tmp = X[i]
