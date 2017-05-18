97a4af9f65b72da8e806f91d4201c3411bc4dc36
festorm <freja.storm@chem.ku.dk>
2017-05-18 08:23:10 +0200

    Bubblesort example

---
+++ lectures_code/week_4/bubblesort.py
@@ -0,0 +1,30 @@
+import numpy as np
+from numba import jit
+import time
+
+start = time.time()
+
+#@jit() #uncomment to use the jit compiler for speed-up
+def bubblesort(X):
+    N = len(X)
+    for end in range(N, 1, -1):
+        for i in range(end - 1):
+            cur = X[i]
+            if cur > X[i + 1]:
+                tmp = X[i]
+                X[i] = X[i + 1]
+                X[i + 1] = tmp
+               
+
+original = np.arange(0.0, 50.0, 0.01)
+shuffled = original.copy()
+np.random.shuffle(shuffled)
+
+print original[0:10]
+print shuffled[0:10]
+
+sorted = shuffled.copy()
+bubblesort(sorted)
+print(np.array_equal(sorted, original))
+
+print 'It took', time.time() - start, 'seconds'
